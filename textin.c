4ec91311a23f4dd6637492295a9a9abe79650459
Martin Kelm <martinkelm@shrt.ws>
2014-11-13 09:50:20 +0100

    improved thread handling

--- textin.c
+++ textin.c
@@ -2,6 +2,7 @@
 #include "textlist.h"
 
 int quit = FALSE;
+int run_espeak = FALSE;
 
 wchar_t input_str[256];
 size_t input_str_len = 0;
@@ -10,17 +11,28 @@ SDL_Thread *thread1 = NULL;
 SDL_mutex *espeak_lock = NULL;
 
 int espeak_thread(void *data) {
-  while (quit == FALSE) {
-    wchar_t espeak_command_w[256] = L"espeak -v mb/mb-de4 -s 90 \"";
+  int local_run_espeak = FALSE;
 
+  while (quit == FALSE) {
     SDL_mutexP(espeak_lock);
-    wcscat(espeak_command_w, textlist_get_current());
+    local_run_espeak = run_espeak;
     SDL_mutexV(espeak_lock);
 
-    wcscat(espeak_command_w, L"\" 2>/dev/null");
-    char espeack_command[256];
-    wcstombs(espeack_command, espeak_command_w, 255);
-    system(espeack_command);
+    if (local_run_espeak == TRUE) {
+      SDL_mutexP(espeak_lock);
+      run_espeak = FALSE;
+      SDL_mutexV(espeak_lock);
+
+      wchar_t espeak_command_w[256] = L"espeak -v mb/mb-de4 -s 90 \"";
+      wcscat(espeak_command_w, textlist_get_current());
+      wcscat(espeak_command_w, L"\" 2>/dev/null");
+      char espeack_command[256];
+      wcstombs(espeack_command, espeak_command_w, 255);
+      system(espeack_command);
+
+    } else {
+      SDL_Delay(1);
+    }
   }
   return 0;
 }
@@ -36,16 +48,19 @@ int main() {
   textlist_set_random_pos();
 
   espeak_lock = SDL_CreateMutex();
-
   thread1 = SDL_CreateThread(espeak_thread, NULL);
 
   while (quit == FALSE) {
+    SDL_mutexP(espeak_lock);
+    run_espeak = TRUE;
+    SDL_mutexV(espeak_lock);
+
     fgetws(input_str, 255, stdin);
     input_str[wcslen(input_str) - 1] = L'\0';
     if (wcscmp(input_str, L"ende") == 0) {
       quit = TRUE;
     } else if (textlist_current_compare(input_str) == 0) {
-      wprintf(L"richtig\n");
+      wprintf(L"--> richtig\n");
 
       SDL_mutexP(espeak_lock);
       textlist_remove_current();
@@ -56,10 +71,8 @@ int main() {
         input_init();
       }
     } else {
-      wprintf(L"falsch\n");
+      wprintf(L"--> falsch\n");
     }
-
-    SDL_Delay(1);
   }
 
   SDL_WaitThread(thread1, NULL);
