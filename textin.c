6945af61b4d2291f4fa79ef87c5d2250796b6b93
Martin Kelm <martinkelm@shrt.ws>
2014-11-13 09:28:29 +0100

    use mutex / condition

--- textin.c
+++ textin.c
@@ -7,30 +7,24 @@ wchar_t input_str[256];
 size_t input_str_len = 0;
 
 SDL_Thread *thread1 = NULL;
-int use_espeak_thread = FALSE;
-SDL_sem *espeak_lock = NULL;
+SDL_mutex *espeak_lock = NULL;
+SDL_cond *can_espeak = NULL;
 
 int espeak_thread(void *data) {
-  int local_use_espeak = FALSE;
   while (quit == FALSE) {
+    SDL_CondWait(can_espeak, espeak_lock);
 
-    SDL_SemWait(espeak_lock);
-    local_use_espeak = use_espeak_thread;
-    SDL_SemPost(espeak_lock);
+    wchar_t espeak_command_w[256] = L"espeak -v mb/mb-de4 -s 90 \"";
 
-    if (local_use_espeak == TRUE) {
-      wchar_t espeak_command_w[256] = L"espeak -v mb/mb-de4 -s 90 \"";
-      wcscat(espeak_command_w, textlist_get_current());
-      wcscat(espeak_command_w, L"\" 2>/dev/null");
-      char espeack_command[256];
-      wcstombs(espeack_command, espeak_command_w, 255);
-      system(espeack_command);
-      SDL_Delay(500);
-    } else {
-      SDL_Delay(1);
-    }
+    SDL_mutexP(espeak_lock);
+    wcscat(espeak_command_w, textlist_get_current());
+    SDL_mutexV(espeak_lock);
+
+    wcscat(espeak_command_w, L"\" 2>/dev/null");
+    char espeack_command[256];
+    wcstombs(espeack_command, espeak_command_w, 255);
+    system(espeack_command);
   }
-
   return 0;
 }
 
@@ -44,29 +38,27 @@ int main() {
   textlist_load();
   textlist_set_random_pos();
 
-  use_espeak_thread = TRUE;
+  espeak_lock = SDL_CreateMutex();
+  can_espeak = SDL_CreateCond();
+
   thread1 = SDL_CreateThread(espeak_thread, NULL);
 
   while (quit == FALSE) {
-
+    SDL_CondSignal(can_espeak);
     fgetws(input_str, 255, stdin);
     input_str[wcslen(input_str) - 1] = L'\0';
     if (wcscmp(input_str, L"ende") == 0) {
       quit = TRUE;
     } else if (textlist_current_compare(input_str) == 0) {
       wprintf(L"richtig\n");
-      SDL_SemWait(espeak_lock);
-      use_espeak_thread = FALSE;
-      SDL_SemPost(espeak_lock);
 
+      SDL_mutexP(espeak_lock);
       textlist_remove_current();
       quit = (textlist_set_random_pos() == TRUE) ? FALSE : TRUE;
+      SDL_mutexV(espeak_lock);
+
       if (quit == FALSE) {
         input_init();
-
-        SDL_SemWait(espeak_lock);
-        use_espeak_thread = TRUE;
-        SDL_SemPost(espeak_lock);
       }
     } else {
       wprintf(L"falsch\n");
@@ -76,7 +68,8 @@ int main() {
   }
 
   SDL_WaitThread(thread1, NULL);
-  SDL_DestroySemaphore(espeak_lock);
+  SDL_DestroyMutex(espeak_lock);
+  SDL_DestroyCond(can_espeak);
 
   return 0;
 }
